28bfb7f04bd49523abe82f458ddb3583c0bc06aa
imjayca <imjay.ca@gmail.com>
2015-07-22 16:32:56 -0700

    NewFilesAdded
    NewFilesAdded

---
+++ JavaScript & jQuery Tutorial for Beginners/JavaScript & jQuery Tutorial for Beginners - 2 of 9 - jQuery Selectors/myscript.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('#testbutton, strong').click(function() {
+		// $('div, strong').css('background-color', 'red');
+		// $('div>p').css('background-color', 'red');
+		// $('div>p:first-child').css('background-color', 'red');
+		// $('div>p:last-child').css('background-color', 'red');
+		// $('div strong').css('background-color', 'red');
+		// $('p:even').css('background-color', 'red');
+		// $('p:odd').css('background-color', 'red');
+		// $('.multiple').css('background-color', 'red');
+		// $('strong.multiple').css('background-color', 'red');
+		$(this).css('background-color', 'red');
+
+	});	
+});
